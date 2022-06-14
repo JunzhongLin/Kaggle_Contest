130d46176abf34473129ba88a419d2cd0b30456b
Yanying Qi <linjunzhongtju@gmail.com>
2022-06-14 14:47:47 +0200

    add src/_private.py, src/correlation_eval.py:
    ------
     add the functionality for the correlation evaluation of categorical variables
    ----
    _private.py contains the basic functions used in higher api
    correlation_eval.py: add Cramer's V method, Theil's U method and correlation ratio method
    ---
    Those methods can be passed to panads.Dataframe.corr to calculate the correlation matrix

---
+++ src/correlation_eval.py
@@ -0,0 +1,104 @@
+# Functions for calculate the correlations of variables are defined in this file
+# Those function can be passed to pandas.Dataframe.corr to generate the corr matrix
+# Source: https://towardsdatascience.com/the-search-for-categorical-correlation-a1cf7f1888c9
+
+import pandas as pd
+import statsmodels as sm
+import numpy as np
+import scipy.stats as ss
+import seaborn as sns
+import math
+from collections import Counter
+from src._private import remove_incomplete_samples, replace_nan_with_value
+
+_REPLACE = 'replace'
+_DROP = 'drop'
+_DROP_SAMPLES = 'drop_samples'
+_DROP_FEATURES = 'drop_features'
+_SKIP = 'skip'
+_DEFAULT_REPLACE_VALUE = 0.0
+_PRECISION = 1e-13
+
+def conditional_entropy(x,
+                        y,
+                        nan_strategy=_REPLACE,
+                        nan_replace_value=_DEFAULT_REPLACE_VALUE,
+                        log_base: float = math.e):
+    """
+    Calculates the conditional entropy of x given y: S(x|y)
+    Wikipedia: https://en.wikipedia.org/wiki/Conditional_entropy
+    Parameters:
+    -----------
+    x : list / NumPy ndarray / Pandas Series
+        A sequence of measurements
+    y : list / NumPy ndarray / Pandas Series
+        A sequence of measurements
+    nan_strategy : string, default = 'replace'
+        How to handle missing values: can be either 'drop' to remove samples
+        with missing values, or 'replace' to replace all missing values with
+        the nan_replace_value. Missing values are None and np.nan.
+    nan_replace_value : any, default = 0.0
+        The value used to replace missing values with. Only applicable when
+        nan_strategy is set to 'replace'.
+    log_base: float, default = e
+        specifying base for calculating entropy. Default is base e.
+    Returns:
+    --------
+    float
+    """
+    if nan_strategy == _REPLACE:
+        x, y = replace_nan_with_value(x, y, nan_replace_value)
+    elif nan_strategy == _DROP:
+        x, y = remove_incomplete_samples(x, y)
+    y_counter = Counter(y)
+    xy_counter = Counter(list(zip(x, y)))
+    total_occurrences = sum(y_counter.values())
+    entropy = 0.0
+    for xy in xy_counter.keys():
+        p_xy = xy_counter[xy] / total_occurrences
+        p_y = y_counter[xy[1]] / total_occurrences
+        entropy += p_xy * math.log(p_y / p_xy, log_base)
+    return entropy
+
+
+def cramers_v(x, y):
+    confusion_matrix = pd.crosstab(x,y)
+    chi2 = ss.chi2_contingency(confusion_matrix)[0]
+    n = confusion_matrix.sum().sum()
+    phi2 = chi2/n
+    r,k = confusion_matrix.shape
+    phi2corr = max(0, phi2-((k-1)*(r-1))/(n-1))
+    rcorr = r-((r-1)**2)/(n-1)
+    kcorr = k-((k-1)**2)/(n-1)
+    return np.sqrt(phi2corr/min((kcorr-1), (rcorr-1)))
+
+
+def theils_u(x, y):
+    s_xy = conditional_entropy(x,y)
+    x_counter = Counter(x)
+    total_occurrences = sum(x_counter.values())
+    p_x = list(map(lambda n: n/total_occurrences, x_counter.values()))
+    s_x = ss.entropy(p_x)
+    if s_x == 0:
+        return 1
+    else:
+        return (s_x - s_xy) / s_x
+
+
+def correlation_ratio(categories, measurements):
+    fcat, _ = pd.factorize(categories)
+    cat_num = np.max(fcat)+1
+    y_avg_array = np.zeros(cat_num)
+    n_array = np.zeros(cat_num)
+    for i in range(0,cat_num):
+        cat_measures = measurements[np.argwhere(fcat == i).flatten()]
+        n_array[i] = len(cat_measures)
+        y_avg_array[i] = np.average(cat_measures)
+    y_total_avg = np.sum(np.multiply(y_avg_array,n_array))/np.sum(n_array)
+    numerator = np.sum(np.multiply(n_array,np.power(np.subtract(y_avg_array,y_total_avg),2)))
+    denominator = np.sum(np.power(np.subtract(measurements,y_total_avg),2))
+    if numerator == 0:
+        eta = 0.0
+    else:
+        eta = np.sqrt(numerator/denominator)
+    return eta
